d5a5189ffeaca96d57a7a6653895934a85b2e3e8
Carl <callmer95@gmail.com>
2017-02-14 20:24:46 +0100

    working on the back-end framework

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -1,26 +1,43 @@
 
 import React from "react"
 
-import Heading from "./Heading"
+import Recipe from "./Recipe"
+
 
 export default class Box extends React.Component {
+	
+	constructor() {
+    super();
+    this.state = {recipes: 
+    	[
+    		{title: "Becon & Egg Sandwich", content: "Bread, Egg, Bacon"},
+    		{title: "A Cheicken Thing", content: "Chicken, Curry, Onions, Cheese"}
+    	]}
+  }
 
 	render() {
 		return (
-			<div class="panel-group">
-			  <div class="panel panel-default">
-			    <div class="panel-heading">
-			    	<Heading/>
-			    </div>
-			    <div id="collapse1" class="panel-collapse collapse">
-			      <ul class="list-group">
-			        <li class="list-group-item">One</li>
-			        <li class="list-group-item">Two</li>
-			        <li class="list-group-item">Three</li>
-			      </ul>
-			    </div>
-			  </div>
+			<div class="panel-group" id="accordion">
+			{
+				this.state.recipes.map((recipe, i) => {
+					return <Recipe key={i} oneRecipe={recipe}/>
+				})	
+			}
 			</div>
 		)
 	}
 }
+
+
+		 //    	{
+		 //    		this.state.recipes.map((recipe, i) => {
+			//     		return <Heading key={i} recipeHeading={recipe} />
+			//     	})
+		 //    	}
+			//    </div>
+			//     {
+		 //    		this.state.recipes.map((recipe, i) => {
+			//     		return <Content key={i} recipeContent={recipe} />
+			//     	})
+		 //    	}
+			// </div>

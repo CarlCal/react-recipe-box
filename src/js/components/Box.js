b322350a1305e2c55ca1566c8a079609d37738f2
Carl <callmer95@gmail.com>
2017-02-15 18:52:29 +0100

    implomenting local storage

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -1,48 +1,35 @@
 
 import { Accordion, Panel, 
-				 ListGroup, ListGroupItem, Modal, 
-				 FormControl, FormGroup, ControlLabel} from 'react-bootstrap';
+				 ListGroup, ListGroupItem} from 'react-bootstrap';
 
 import React from "react"
 
 // Figure out cache and passport, sessionsjs
 
+import popModule from "./popModal"
+
 export default class Box extends React.Component {
 	
 	constructor() {
-    super();
-    this.state = {
-    	recipes: 
-    	[
-    		{title: "Becon & Egg Sandwich", ingredients: ["Bread", "Egg", "Bacon"]},
-    		{title: "A Chicken Thing", ingredients: ["Chicken", "Curry", "Onions", "Cheese"]}
-    	],
-    	showModal: false
-    }
+    super()
+    this.state = {parsedLocalStorage: [], showAddModal: false, showEditModal: false}
   }
 
-  handleSubmit(event) {
-
-  }
-
-  handleChange(event) {
-
-  }
-
-  close() {
-    this.setState({ showModal: false });
-  }
-
-  open() {
-    this.setState({ showModal: true });
+  componentDidMount() {
+  	window.localStorage.setItem("carlcRecipes", JSON.stringify([{title: "Becon & Egg Sandwich", ingredients: ["Bread", "Egg", "Bacon"]},
+  																														  {title: "A Chicken Thing", ingredients: ["Chicken", "Curry", "Onions", "Cheese"]}]))
+    
+  	var storage = JSON.parse(localStorage.getItem("carlcRecipes"));
+    this.setState({parsedLocalStorage: storage})	
   }
 
 	render() {
+		
 		return (
 			<div>
 			  <Accordion>
 			    {
-			    	this.state.recipes.map((recipe, i) => {
+			    	this.state.parsedLocalStorage.map((recipe, i) => {
 			    		return <Panel id="panel" bsStyle="success" header={recipe.title} key={i} eventKey={i}>
 			    						<div id="title-container" >
 			    							<p>{"Ingredients"}</p>
@@ -62,44 +49,14 @@ export default class Box extends React.Component {
 			    	})
 			    }
 			  </Accordion>
-
 			  <div>
-			  <button class="btn btn-lg btn-info" onClick={this.open.bind(this)}>Add Recipe</button>
-			  <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
-	          <Modal.Header closeButton>
-	            <center><Modal.Title>Add a new recipe</Modal.Title></center>
-	          </Modal.Header>
-
-	          <form>
-		          <Modal.Body>
-					        <FormGroup>
-					          <ControlLabel>Recipe:</ControlLabel>
-					          <FormControl
-					          	onChange={this.handleChange.bind(this)}
-					          	name= "title"
-					            type="text"
-					            placeholder="Recipe Name"
-					          />
-					        </FormGroup>
-					        <FormGroup>
-					          <ControlLabel>Ingredients:</ControlLabel>
-					          <FormControl
-					          	onChange={this.handleChange.bind(this)}
-					          	name= "ingredients"
-					            componentClass="textarea"
-					            placeholder="Enter ingredients,seperated,with,commas"
-					          />
-					        </FormGroup>
-		          </Modal.Body>
-
-		          <Modal.Footer>
-		          	<button type="reset" onClick={this.handleSubmit.bind(this)} class="btn btn-sm btn-primary" >Add Recipe</button>
-		          </Modal.Footer>
-	          </form>
-
-        </Modal>
+			  	<button class="btn btn-lg btn-info">Add Recipe</button>
 			  </div>
 			</div>
 		)
 	}
 }
+
+			  	// <popModal 
+			  	// 	modalAction="Add Recipe" 
+			  	// 	getModal={this.state.showAddModal}/>
--- src/js/components/popModal.js
+++ src/js/components/popModal.js
@@ -1,23 +1,24 @@
 
 import React from "react"
 
-import { Modal, FormControl, FormGroup, ControlLabel} from 'react-bootstrap';
+import { Modal, FormControl, FormGroup, ControlLabel } from 'react-bootstrap'
 
-export default class addRecipe extends {
+export default class popModal extends React.Component {
 
-	constructor() {
-    super();
-    this.state = {
-    	showModal: false,
-    	ingredientsValidation: ""}
+	close() {
+    //this.setState({ showAddModal: false });
+  }
+
+  open() {
+    //this.setState({ showAddModal: true });
   }
 
 	render() {
 		return (
 			<div>
-						  <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+			  <Modal show={this.props.getModal} onHide={this.close.bind(this)}>
 	          <Modal.Header closeButton>
-	            <Modal.Title>Add a new recipe</Modal.Title>
+	            <center><Modal.Title>Add a new recipe</Modal.Title></center>
 	          </Modal.Header>
 
 	          <form>
@@ -25,6 +26,7 @@ export default class addRecipe extends {
 					        <FormGroup>
 					          <ControlLabel>Recipe:</ControlLabel>
 					          <FormControl
+					          	name= "title"
 					            type="text"
 					            placeholder="Recipe Name"
 					          />
@@ -32,6 +34,7 @@ export default class addRecipe extends {
 					        <FormGroup>
 					          <ControlLabel>Ingredients:</ControlLabel>
 					          <FormControl
+					          	name= "ingredients"
 					            componentClass="textarea"
 					            placeholder="Enter ingredients,seperated,with,commas"
 					          />
@@ -39,10 +42,10 @@ export default class addRecipe extends {
 		          </Modal.Body>
 
 		          <Modal.Footer>
-		          	<button type="submit" /*onClick*/ class="btn btn-sm btn-primary" >Add Recipe</button>
+		          	<button type="reset" class="btn btn-sm btn-primary" >{this.props.modalAction}</button>
 		          </Modal.Footer>
 	          </form>
-	          
+
         </Modal>
         </div>
 		)

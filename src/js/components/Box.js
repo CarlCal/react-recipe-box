13e8cd0909a0b63f3bbb9656e5e40781917c5989
Carl <callmer95@gmail.com>
2017-02-18 09:06:31 +0100

    starting to look like i whant it too

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -4,14 +4,26 @@ import React from "react"
 export default class Body extends React.Component {
 	render() {
 		return (
-			<ul id="box" class="flex-container">
-			  <li class="flex-item">
-			  	<div class="recipe-display">
-			  		<div class="recipe-title"></div>
-			  		<img class="recipe-img" src="http://www.thebalkanbackpacker.com/wp-content/uploads/2015/03/serbian-food.jpg" alt="" />
-			  	</div>
-			  </li>
-			</ul>
+			<div id="card-holder">
+				<div id="pole"></div>
+				<div class="flex-container">
+					<div class="flex-item">
+						<div class="label">
+							<p>Test Recipe</p>
+						</div>
+						<div class="conect-pin"></div>
+					</div>
+					<div class="flex-item">
+						<div class="label">
+							<p>Anouther longer Recipe</p>
+						</div>
+						<div class="conect-pin"></div>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
+
+
+

bac732f2a05b13ef0edd791cc5492d33031ba547
Carl <callmer95@gmail.com>
2017-02-19 10:28:03 +0100

    the dumb buttons works now

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -1,22 +1,28 @@
 
+import $ from "jquery"
+
 import React from "react"
 
 export default class Body extends React.Component {
-	render() {
+
+handleSelection(event) {
+	var target = event.target
+	console.log(event.target)
+
+	$("label:checked + .label").css("background-color", "green")
+}
+
+render() {
 		return (
 			<div id="card-holder">
 				<div id="pole"></div>
 				<div class="flex-container">
-					<div class="flex-item">
-						<div class="label">
-							<p>Test Recipe</p>
-						</div>
+					<div class="flex-item" >
+						<button class="btn btn-info cell">Recepie Test</button>
 						<div class="conect-pin"></div>
 					</div>
-					<div class="flex-item">
-						<div class="label">
-							<p>Anouther longer Recipe</p>
-						</div>
+					<div  class="flex-item" >
+						<button class="btn btn-info cell">A long test example with mroe caracters sdfs</button>
 						<div class="conect-pin"></div>
 					</div>
 				</div>
@@ -25,5 +31,8 @@ export default class Body extends React.Component {
 	}
 }
 
+
+//max 44 caracters in recipe name
+//23 recipes
 
 

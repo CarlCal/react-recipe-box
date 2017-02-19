57f20a2a851609e429a75ab80fec52df30869a9e
Carl <callmer95@gmail.com>
2017-02-19 12:27:26 +0100

    layout changes

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -14,16 +14,23 @@ handleSelection(event) {
 
 render() {
 		return (
-			<div id="card-holder">
-				<div id="pole"></div>
-				<div class="flex-container">
-					<div class="flex-item" >
-						<button class="btn btn-info cell">Recepie Test</button>
-						<div class="conect-pin"></div>
+			<div>
+				<div id="card-holder">
+					<div id="pole"></div>
+					<div class="flex-container">
+						<div class="flex-item" >
+							<button class="btn btn-info cell">Recepie Test</button>
+							<div class="conect-pin"></div>
+						</div>
+						<div  class="flex-item" >
+							<button class="btn btn-info cell">A long test example with mroe caracters sdfs</button>
+							<div class="conect-pin"></div>
+						</div>
 					</div>
-					<div  class="flex-item" >
-						<button class="btn btn-info cell">A long test example with mroe caracters sdfs</button>
-						<div class="conect-pin"></div>
+				</div>
+				<div id="card-viewer" >
+					<div id="display" class="container">
+						
 					</div>
 				</div>
 			</div>
@@ -31,6 +38,7 @@ render() {
 	}
 }
 
+//<img class="recipe-img" src="http://68.media.tumblr.com/71a040174d38fc95bcaccdda59d06cf8/tumblr_mznzmrod2c1qfj6wko1_500.jpg" alt="" />
 
 //max 44 caracters in recipe name
 //23 recipes
